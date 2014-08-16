abd0edcd16034f663418f5ca86f5a38ff75a73cc
parizeau <marc@parizeau.org>
2014-08-16 15:38:57 -0400

    Updated for the arff only data format.

--- DataIO.py
+++ DataIO.py
@@ -1,57 +1,26 @@
-import csv, sys
+import arff
 
-def readData(iFilename, iFormat):
-    """
-    Read the data file.
+def read(iFilename):
+    """ Read data in arff format. """
 
-    Returns a list of tuples containing a numpy array and.
-    """
-    try:
-        if iFilename == 'stdin':
-            import sys
-            lFD = sys.stdin
-        else:
-            lFD = open(iFilename)
-    except IOError:
-        print('\aError, cannot open file : ', iFilename)
-        exit()
-
-    if iFormat == 'csv':
-        import csv
-        lFile = csv.reader(lFD)
-    elif iFormat == 'arff':
-        import arff
-        lFile = arff.Reader(lFD)
-    else: 
-        print("\aError, invalid format: ", iFormat)
-        exit()
-
-    lData = []
-    for lRow in lFile:
-        # skip header row
-        if list(lRow)[-1] == 'label': continue
-
-        # create tuple (array, label)
-        lData.append(list(lRow))
-
-    return lData
-
-def writeData(iHeader, iData, iFormat):
-    """ Write output file in specified format. """
-
-    if iFormat == 'csv':
-        iData.insert(0, [x[0] for x in iHeader['attrs']])
-    elif iFormat == 'arff':
-        print('% Flying Gaussians, Marc Parizeau, 2014')
-        print('\n@relation', iHeader['filename'])
-        print()
-        for n, t in iHeader['attrs']:
-            print("@attribute", n, t)
-        print('\n@data')
+    if iFilename == '-':
+        import sys
+        lFD = sys.stdin
     else:
-        print("\aError, invalid format: ", iFormat)
-        exit()
+        try:
+            lFD = open(iFilename)
+        except:
+            lFD = open(iFilename+'.arff')
 
-    lFile = csv.writer(sys.stdout)
-    for lRow in iData:
-        lFile.writerow(lRow)
+    return arff.load(lFD)
+
+def write(iRelation, iAttrs, iData):
+    """ Write data in arff format. """
+
+    lInfo = {}
+    lInfo['description'] = "\nFlying non-stationary gaussians, 2014\n"
+    lInfo['relation'] = iRelation
+    lInfo['attributes'] = iAttrs
+    lInfo['data'] = iData
+
+    print(arff.dumps(lInfo))
